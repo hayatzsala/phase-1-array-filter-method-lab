f6abf6883e5e4e8a1fe9b8e3e43e13c4a2bad5da
hayatSalawdeh <salawdeh.hayat@gmail.com>
2022-08-27 14:59:28 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(arr,name){
+    return arr.filter((element)=>{
+        return element.toUpperCase() === name.toUpperCase();
+    })
+}
+function fuzzyMatch(arr,nameStart){
+    return arr.filter((element)=> {
+        return element.startsWith(nameStart);
+    })
+}
+
+function matchName(arr, name){
+    return arr.filter((element)=>{
+        return element.name === name;
+    })
+}
